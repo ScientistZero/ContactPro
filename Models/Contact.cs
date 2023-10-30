18ac1be59ad5c09963ef7cb0ca2a72ab5dfe5d8b
Chris Herzig <81333676+ScientistZero@users.noreply.github.com>
2023-10-30 08:14:54 -0400

    Adding Models

---
+++ Models/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ContactPro.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? AppUserID { get; set; }
+        [Required]
+        [Display(Name ="First Name")]
+        [StringLength(50,ErrorMessage ="The {0} must be at least {2} and a max of {1} characters long.",MinimumLength =2)]
+        public string? FirstName   { get; set; }
+
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and a max of {1} characters long.", MinimumLength = 2)]
+        public string? LastName { get; set; }
+
+    }
+}
